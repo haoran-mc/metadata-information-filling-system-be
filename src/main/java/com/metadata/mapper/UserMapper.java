a99e95a740bbb104afb2b3cd18a929c46722b8b8
haoran <haoran.mc@outlook.com>
2021-12-19 09:56:58 +0800

    :construction:添加用户mybatis mapper

---
+++ src/main/java/com/metadata/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.metadata.mapper;
+
+import com.metadata.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Mapper
+@Repository
+public interface UserMapper {
+    List<User> queryUserList();
+}
