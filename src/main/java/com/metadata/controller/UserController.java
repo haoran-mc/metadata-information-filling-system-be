98b158ca55936ab363a85389bb4b0a8be70f6b29
haoran <haoran.mc@outlook.com>
2021-12-19 09:56:40 +0800

    :construction:添加用户查询接口

---
+++ src/main/java/com/metadata/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.metadata.controller;
+
+import com.metadata.entity.User;
+import com.metadata.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/queryUserList")
+    public List<User> queryUserList() {
+        List<User> userList = userMapper.queryUserList();
+        for (User user : userList) {
+            System.out.println(user);
+        }
+        return userList;
+    }
+}
