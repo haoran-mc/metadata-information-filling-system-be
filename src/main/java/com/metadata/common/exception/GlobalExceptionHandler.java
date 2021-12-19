1b02a16be6b5fedee0d7719a296b258ea5547a9d
haoran <haoran.mc@outlook.com>
2021-12-19 22:18:33 +0800

    :sparkles:自定义的异常类，用来判断异常

---
+++ src/main/java/com/metadata/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.metadata.common.exception;
+
+
+import com.metadata.common.lang.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// 异步
+@RestControllerAdvice
+// 日志
+@Slf4j
+public class GlobalExceptionHandler {
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public Result handler(MethodArgumentNotValidException e) {
+        log.error("实体校验异常：", e);
+
+        BindingResult bindingResult = e.getBindingResult();
+        ObjectError objectError = bindingResult.getAllErrors().stream().findFirst().get();
+
+        return Result.fail(objectError.getDefaultMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = IllegalArgumentException.class)
+    public Result handler(IllegalArgumentException e) {
+        log.error("Assert 异常：", e);
+        return Result.fail(e.getMessage());
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(value = RuntimeException.class)
+    public Result handler(RuntimeException e) {
+        log.error("运行时异常：", e);
+        return Result.fail(e.getMessage());
+    }
+}
