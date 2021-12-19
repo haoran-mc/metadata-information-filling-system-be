05792e6441cc8408cd8205553d8fb3207e807441
haoran <haoran.mc@outlook.com>
2021-12-19 09:58:08 +0800

    :construction:启动类

---
+++ src/main/java/com/metadata/MetadataInformationFillingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.metadata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetadataInformationFillingSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MetadataInformationFillingSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/metadata/MetadataInformationFillingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.metadata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetadataInformationFillingSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
