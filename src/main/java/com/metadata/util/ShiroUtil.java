453cbf03c64309c49c755f973e21be92f4d54f36
haoran <haoran.mc@outlook.com>
2021-12-19 22:22:01 +0800

    :sparkles:shiro工具类

---
+++ src/main/java/com/metadata/util/ShiroUtil.java
@@ -0,0 +1,10 @@
+package com.metadata.util;
+
+import com.metadata.shiro.AccountProfile;
+import org.apache.shiro.SecurityUtils;
+
+public class ShiroUtil {
+    public static AccountProfile getProfile() {
+        return (AccountProfile) SecurityUtils.getSubject().getPrincipal();
+    }
+}
