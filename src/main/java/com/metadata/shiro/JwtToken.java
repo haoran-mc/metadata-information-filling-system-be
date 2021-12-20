e0b549fc815556be6b79fa255803eef7be6cdc71
haoran <haoran.mc@outlook.com>
2021-12-20 18:47:54 +0800

    :lipstick:代码格式

--- src/main/java/com/metadata/shiro/JwtToken.java
+++ src/main/java/com/metadata/shiro/JwtToken.java
@@ -4,6 +4,7 @@ import org.apache.shiro.authc.AuthenticationToken;
 
 public class JwtToken implements AuthenticationToken {
     private String token;
+
     public JwtToken(String jwt) {
         this.token = jwt;
     }
