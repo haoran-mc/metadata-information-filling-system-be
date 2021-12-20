a4abf7e927571b73b140cecdb691b993ded74ce0
haoran <haoran.mc@outlook.com>
2021-12-20 18:42:59 +0800

    :fire:修改携带的帐号信息

--- src/main/java/com/metadata/shiro/AccountProfile.java
+++ src/main/java/com/metadata/shiro/AccountProfile.java
@@ -6,11 +6,8 @@ import java.io.Serializable;
 
 @Data
 public class AccountProfile implements Serializable {
-    private Long id;
-
     private String username;
 
-    private String avatar;
-
-    private String email;
+    private String nickname;
+    // private int identify;
 }
