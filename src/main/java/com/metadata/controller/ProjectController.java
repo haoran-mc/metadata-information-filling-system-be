644756de677623b826c47f8bdccd1b1c52a0d730
haoran <haoran.mc@outlook.com>
2021-12-20 10:21:46 +0800

    :art:添加controller层

---
+++ src/main/java/com/metadata/controller/ProjectController.java
@@ -0,0 +1,9 @@
+package com.metadata.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/projects")
+public class ProjectController {
+}
--- src/main/java/com/metadata/controller/UserController.java
+++ src/main/java/com/metadata/controller/UserController.java
@@ -2,20 +2,24 @@ package com.metadata.controller;
 
 import com.metadata.entity.User;
 import com.metadata.mapper.UserMapper;
+import com.metadata.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+@RequestMapping("/users")
 public class UserController {
     @Autowired
-    private UserMapper userMapper;
+    private UserService userService;
 
-    @GetMapping("/queryUserList")
-    public List<User> queryUserList() {
-        List<User> userList = userMapper.queryUserList();
+    @GetMapping("/all")
+    public List<User> getAllUser() {
+        System.out.println("--------------------------");
+        List<User> userList = userService.getAllUsers();
         for (User user : userList) {
             System.out.println(user);
         }
