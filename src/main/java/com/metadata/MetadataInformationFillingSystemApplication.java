4a88d84c998f04efad28b6fbf85d9c56178a3ac5
haoran <haoran.mc@outlook.com>
2021-12-20 10:26:53 +0800

    :wrench:扫描mapper下面的mapper

--- src/main/java/com/metadata/MetadataInformationFillingSystemApplication.java
+++ src/main/java/com/metadata/MetadataInformationFillingSystemApplication.java
@@ -2,8 +2,12 @@ package com.metadata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@ComponentScan(basePackages = {"com.metadata.mapper"})
 public class MetadataInformationFillingSystemApplication {
 
     public static void main(String[] args) {
