67a9c68bd697ce0d5485d498fad084f08d003f95
haoran <haoran.mc@outlook.com>
2021-12-20 10:22:10 +0800

    :art:添加service层

--- src/main/java/com/metadata/service/UserService.java
+++ src/main/java/com/metadata/service/UserService.java
@@ -3,6 +3,10 @@ package com.metadata.service;
 import com.metadata.entity.User;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public interface UserService {
     public User getById(int id);
+    public List<User> getAllUsers();
 }
--- src/main/java/com/metadata/service/impl/UserServiceImpl.java
+++ src/main/java/com/metadata/service/impl/UserServiceImpl.java
@@ -1,13 +1,26 @@
 package com.metadata.service.impl;
 
 import com.metadata.entity.User;
+import com.metadata.mapper.UserMapper;
 import com.metadata.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
+    @Autowired
+    UserMapper userMapper;
+
+    @Override
     public User getById(int id) {
         User user = new User();
         return user;
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return userMapper.queryUserList();
     }
 }
