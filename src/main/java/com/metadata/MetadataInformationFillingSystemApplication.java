091c77def7fc3bd4da860ad697503cffc2d4db00
haoran <haoran.mc@outlook.com>
2021-12-20 18:49:15 +0800

    :bug:不需要多次扫描mapper文件

--- src/main/java/com/metadata/MetadataInformationFillingSystemApplication.java
+++ src/main/java/com/metadata/MetadataInformationFillingSystemApplication.java
@@ -2,16 +2,10 @@ package com.metadata;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-//@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
-@ComponentScan(basePackages = {"com.metadata.mapper"})
 public class MetadataInformationFillingSystemApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(MetadataInformationFillingSystemApplication.class, args);
     }
-
 }
