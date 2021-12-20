3fc4e944906e43f99ee262d8c66e6c8079690f10
haoran <haoran.mc@outlook.com>
2021-12-20 18:51:41 +0800

    :art:完成textbook controller层的代码

--- src/main/java/com/metadata/controller/TextController.java
+++
@@ -1,9 +0,0 @@
-package com.metadata.controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/textbooks")
-public class TextController {
-}
