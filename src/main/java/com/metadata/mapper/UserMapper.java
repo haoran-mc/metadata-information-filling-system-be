bc603a907d82e4dfd03994ec647e02a7999b1fbd
WalterOrient <ZhangZH131@163.com>
2021-12-21 09:47:36 +0800

    :sparkles:新增了用户查询和更新资料mapper

--- src/main/java/com/metadata/mapper/UserMapper.java
+++ src/main/java/com/metadata/mapper/UserMapper.java
@@ -10,4 +10,18 @@ import java.util.List;
 //@Repository
 public interface UserMapper {
     List<User> queryUserList();
+
+    /***
+     * 根据用户名获取用户的全部资料
+     * @param username
+     * @return 如果找到返回对象，反之返回null
+     */
+    User getByUsername(String username);
+
+    /**
+     * 更新用户的数据信息
+     * @param user 用户的数据
+     * @return 返回受影响的行数
+     */
+    int updateInfoByUsername(User user);
 }
