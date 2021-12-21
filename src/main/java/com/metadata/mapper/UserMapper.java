1cd61972053b88ff51ea38aeb607c58891655766
WalterOrient <ZhangZH131@163.com>
2021-12-21 20:35:55 +0800

    :rocket:实现了user相关操作mapper

--- src/main/java/com/metadata/mapper/UserMapper.java
+++ src/main/java/com/metadata/mapper/UserMapper.java
@@ -1,5 +1,7 @@
 package com.metadata.mapper;
 
+import com.metadata.entity.Project;
+import com.metadata.entity.Textbook;
 import com.metadata.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
@@ -7,21 +9,33 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Mapper
-//@Repository
 public interface UserMapper {
-    List<User> queryUserList();
-
-    /***
-     * 根据用户名获取用户的全部资料
-     * @param username
-     * @return 如果找到返回对象，反之返回null
+    /**
+     * 通过用户手机号查询用户个人的资料mapper
+     * @param phone 手机号
+     * @return user对象
      */
-    User getByUsername(String username);
+    User getUserByPhone(String phone);
 
     /**
-     * 更新用户的数据信息
-     * @param user 用户的数据
-     * @return 返回受影响的行数
+     * 通过用户对象更新用户个人的资料mapper
+     * @param phone 手机号
+     * @param username 用户名
+     * @param password 密码
      */
-    int updateInfoByUsername(User user);
+    void updateUserByPhone(String phone, String username, String password);
+
+    /**
+     * 获取用户所填写项目的详细信息mapper
+     * @param phone 手机号
+     * @return project对象
+     */
+    Project getUserBatchProject(String phone);
+
+    /**
+     * 获取用户所填写书籍的详细信息mapper
+     * @param phone 手机号
+     * @return textbook对象
+     */
+    Textbook getUserBatchTextbook(String phone);
 }
