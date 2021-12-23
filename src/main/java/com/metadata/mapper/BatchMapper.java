305a400642990db20e64c3fa498ea37de319116d
WalterOrient <ZhangZH131@163.com>
2021-12-23 09:43:23 +0800

    :rocket:实现了batch Api的mapper

---
+++ src/main/java/com/metadata/mapper/BatchMapper.java
@@ -0,0 +1,41 @@
+package com.metadata.mapper;
+
+import com.metadata.entity.Project;
+import com.metadata.entity.Textbook;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface BatchMapper {
+
+    /**
+     * 获取指定批次的项目mapper
+     * @param year 年份
+     * @param batch 批次
+     * @return project对象集
+     */
+    List<Project> getBatchProject(int year, int batch);
+
+    /**
+     * 获取指定批次的书籍mapper
+     * @param year 年份
+     * @param batch 批次
+     * @return textbook对象集
+     */
+    List<Textbook> getBatchTextbook(int year, int batch);
+
+    /**
+     * 插入新项目mapper
+     * @param project project对象
+     */
+    void createProject(@Param("project") Project project);
+
+    /**
+     * 插入新书籍mapper
+     * @param textbook textbook对象
+     */
+    void createTextbook(@Param("textbook") Textbook textbook);
+
+}
