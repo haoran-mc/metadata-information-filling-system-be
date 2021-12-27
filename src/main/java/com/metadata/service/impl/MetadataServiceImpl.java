15bbe7cef57a809d3bed1f9483b0e078ff584656
haoran <haoran.mc@outlook.com>
2021-12-27 14:25:34 +0800

    :construction:更名

--- src/main/java/com/metadata/service/impl/MetadataServiceImpl.java
+++ src/main/java/com/metadata/service/impl/MetadataServiceImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service("metadataService")
+@Service
 public class MetadataServiceImpl implements MetadataService {
     @Autowired
     MetadataMapper metadataMapper;
@@ -37,21 +37,19 @@ public class MetadataServiceImpl implements MetadataService {
      */
     @Override
     public void insertSpecialData(Project project, Textbook textbook, Batch batch) {
-        if(project != null){    //这里也许需要改，可能出现project/textbook数据为{}的情况，不一定{}这种数据就是null，否则会多插入数据库很多空记录
-            batchMapper.createProject(project);
+        if (project != null) {    // 这里也许需要改，可能出现project/textbook数据为{}的情况，不一定{}这种数据就是null，否则会多插入数据库很多空记录
+            batchMapper.addProject(project);
         }
-        if(textbook != null){
-            batchMapper.createTextbook(textbook);
+        if (textbook != null) {
+            batchMapper.addTextbook(textbook);
         }
-        metadataMapper.createBatch(batch);
+        metadataMapper.addBatch(batch);
         //插入sp表
         Sp sp = new Sp();
         sp.setYear(batch.getYear()); //获取新建批次year
-        sp.setBatch(batch.getBatch_idx()); //获取新建批次batch
-        sp.setProject_id(metadataMapper.getLatestProjectId()); //获取新插入project表的特殊数据的id
-        sp.setTextbook_id(metadataMapper.getLatestTextbookId());//获取新插入textbook表的特殊数据的id
-        metadataMapper.createSp(sp);
+        sp.setBatch_idx(batch.getBatch_idx()); //获取新建批次batch
+        sp.setProject_id(metadataMapper.getSpProjectId()); //获取新插入project表的特殊数据的id
+        sp.setTextbook_id(metadataMapper.getSpTextbookId());//获取新插入textbook表的特殊数据的id
+        metadataMapper.addSp(sp);
     }
-
-
 }
