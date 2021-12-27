3ddad7a660df38556cbd7b54b292ba70816becca
haoran <haoran.mc@outlook.com>
2021-12-27 14:22:43 +0800

    :wheelchair:与类名一样的话就不需要配置了

--- src/main/java/com/metadata/service/impl/BatchServiceImpl.java
+++ src/main/java/com/metadata/service/impl/BatchServiceImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 
-@Service("batchService")
+@Service
 public class BatchServiceImpl implements BatchService {
     @Autowired
     BatchMapper batchMapper;
