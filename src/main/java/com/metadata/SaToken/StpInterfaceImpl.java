a845393f4ea57b4dac6dfca27e424387b7124cd8
haoran <haoran.mc@outlook.com>
2021-12-30 21:20:25 +0800

    :sparkles:配置Sa-Token

---
+++ src/main/java/com/metadata/SaToken/StpInterfaceImpl.java
@@ -0,0 +1,36 @@
+package com.metadata.SaToken;
+
+import cn.dev33.satoken.stp.StpInterface;
+import com.metadata.entity.User;
+import com.metadata.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class StpInterfaceImpl implements StpInterface {
+    @Autowired
+    UserService userService;
+
+    @Override
+    public List<String> getPermissionList(Object loginId, String loginType) {
+        System.out.println("--");
+        return null;
+    }
+
+    @Override
+    public List<String> getRoleList(Object loginId, String loginType) {
+        User user = userService.getUserById((int)loginId);
+
+        System.out.println("role: " + user.getRole());
+
+        List<String> list = new ArrayList<String>();
+
+        System.out.println("--" + user.getRole());
+
+        list.add(user.getRole());
+        return list;
+    }
+}
